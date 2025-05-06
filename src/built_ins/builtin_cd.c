0ae81404c7fba147dfb069213403f1ae8be21dff
Claudia <sofiagil2288@gmail.com>
2025-05-06 16:43:19 +0200

    arreglo extra de cd

--- src/built_ins/builtin_cd.c
+++ src/built_ins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: claudia <claudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 16:18:52 by claudia           #+#    #+#             */
-/*   Updated: 2025/05/06 16:31:36 by claudia          ###   ########.fr       */
+/*   Updated: 2025/05/06 16:42:55 by claudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,60 +33,6 @@ static char	*expand_path(char *arg, t_status *status)
 	ft_strlcat(expanded, arg + 1, total_len);
 	return (expanded);
 }
-
-/*int	builtin_cd(char **args, t_status *status)
-{
-	char	*oldpwd;
-	char	*newpwd;
-	char	*target;
-	char	*expanded = NULL;
-
-	oldpwd = getcwd(NULL, 0);
-	if (!oldpwd)
-		return (perror("getcwd"), 1);
-
-	if (!args[1]) // cd → HOME
-	{
-		target = get_env_value(status->envp, "HOME");
-		if (!target)
-			return (free(oldpwd), fprintf(stderr, "cd: HOME not set\n"), 1);
-	}
-	else if (strcmp(args[1], "-") == 0) // cd -
-	{
-		target = get_env_value(status->envp, "OLDPWD");
-		if (!target)
-			return (free(oldpwd), fprintf(stderr, "cd: OLDPWD not set\n"), 1);
-		if (chdir(target) != 0)
-			return (free(oldpwd), perror("cd"), 1);
-		printf("%s\n", target);
-	}
-	else
-	{
-		expanded = expand_path(args[1], status);
-		if (!expanded)
-			return (free(oldpwd), fprintf(stderr, "cd: error expanding path\n"), 1);
-		if (chdir(expanded) != 0)
-			return (free(oldpwd), free(expanded), perror("cd"), 1);
-	}
-
-	// Si no hemos hecho chdir aún (caso HOME o "-"), hacerlo ahora
-	if (!args[1] || strcmp(args[1], "-") == 0)
-	{
-		if (chdir(target) != 0)
-			return (free(oldpwd), perror("cd"), 1);
-	}
-
-	newpwd = getcwd(NULL, 0);
-	if (!newpwd)
-		return (free(oldpwd), free(expanded), perror("getcwd"), 1);
-
-	status->envp = set_env_value(status->envp, "OLDPWD", oldpwd);
-	status->envp = set_env_value(status->envp, "PWD", newpwd);
-	free(oldpwd);
-	free(newpwd);
-	free(expanded);
-	return (0);
-}*/
 
 static int	change_dir(const char *path)
 {
